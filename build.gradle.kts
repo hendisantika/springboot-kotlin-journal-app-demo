93c33c18cce210e79b70ae64b49cfd31a8fb4e30
Hendi Santika <hendisantika@gmail.com>
2020-02-06 06:09:13 +0700

    #2 - Add required libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("mysql:mysql-connector-java")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
